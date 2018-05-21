5132df8082a6a2766faee985f34deb9f2287bf20
Mikael <mikellallison@gmail.com>
2018-05-21 14:22:06 +0100

    Lambda refactor
    The GO lambda runtime takes a distribution as opposed to a single
    function.  Therefore it's possible to to split operations out to
    packages.

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # k8s-master-create-etcd-ec2-snapshot
 Lambda function to create snapshots of etcd volumes attached to master nodes
+
+
+The below might allow a nicer interface to the TagResource function
+
+```
+func TagResource(resourceID string, tags map[string]string) {
+	tags := []*ec2.Tag{}
+	for key, value := range tagsMap {
+		tags = tags.append(
+			ec2.Tag{
+				Key:   aws.String(key),
+				Value: aws.String(value),
+			}
+		)
+	}
+
+
+	tagsInput := &ec2.CreateTagsInput{
+		Resources: []*string{
+			aws.String(resourceID),
+		},
+		Tags: tags,
+	}
+
+	// Finally tag snapshot with instance ID, Device designation and Volume name
+	tagIt, err := connection.CreateTags(tagsInput)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+	fmt.Println(tagIt)
+
+}
+
+```
---
+++ tags/tags.go
@@ -0,0 +1,70 @@
+package tags
+
+import (
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+)
+
+var connection = ec2.New(session.New())
+
+func FetchResourceTags(resourceID *string) (getTagsOutput *ec2.DescribeTagsOutput) {
+
+	volTagsInput := &ec2.DescribeTagsInput{
+		Filters: []*ec2.Filter{
+			{
+				Name: aws.String("resource-id"),
+				Values: []*string{
+					aws.String(*resourceID),
+				},
+			},
+		},
+	}
+	// Get the volume's tags
+	getTagsOutput, err := connection.DescribeTags(volTagsInput)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	return getTagsOutput
+
+}
+
+func TagResource(resourceID *string, tags []*ec2.Tag) {
+
+	tagsInput := &ec2.CreateTagsInput{
+		Resources: []*string{
+			aws.String(*resourceID),
+		},
+		Tags: tags,
+	}
+
+	tagIt, err := connection.CreateTags(tagsInput)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+	fmt.Println(tagIt)
+
+}
